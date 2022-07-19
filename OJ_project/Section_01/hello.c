125c47c1d344b4f1e090d485730da97f2c80ede7
KimYutan <70746658+yutan0565@users.noreply.github.com>
2022-07-19 22:57:14 +0900

    oj_proeject

---
+++ OJ_project/Section_01/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello Yutan!\n");
+	return 0;
+}
---
+++ OJ_project/Section_02/var.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+	
+	int a;
+	a = 3;
+	printf("%d\n", a);
+	printf("%c\n", 'b');
+	printf("%u\n", 123);
+	printf("%f\n", 12.1);
+	printf("%X\n", 15);
+}
