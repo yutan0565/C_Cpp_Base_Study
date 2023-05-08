d3c74da248353c3c92db4c328e11b39bf0e8f8de
KimYutan <yutan056575@gmail.com>
2023-05-09 00:12:59 +0900

    Create test_01.cc

---
+++ TCP_Cpp/02_타입/test_01.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    // type name
+    int num; // 4 byte วาด็
+    num = 10;
+
+    int num1 = 1, num2 = 12.8;
+    double num3 = 1.23, num4 = 4.56;
+
+    std::cout <<num<<"\n"<<num1<<"\n"<<num2<<"\n"<<num3<<"\n"<<num4<<"\n";
+    return 0;
+
+
+}
