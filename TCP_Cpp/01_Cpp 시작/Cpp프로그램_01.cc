9076e732d59be78a01701bec82343295560db909
KimYutan <yutan056575@gmail.com>
2023-05-08 22:05:01 +0900

    Create Cpp프로그램_01.cc

---
+++ TCP_Cpp/01_Cpp 시작/Cpp프로그램_01.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    while(true)
+    {
+        cout << "나이를 입력 해주세요 : ";
+        cin >> age;
+
+        cout << "내 나이는 " << age << "살 입니다." << endl;
+    }
+    23return 0;
+}
